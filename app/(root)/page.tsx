b64456188aba929b127cedb8e499016dbd35bb2e
Clinzho1996 <confidinho@yahoo.com>
2025-02-11 09:05:45 +0100

    login restructure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -37,50 +37,63 @@ export default function SignIn() {
 	};
 
 	return (
-		<div className="hero bg-primary-5 flex justify-center items-center align-middle h-screen">
-			<div className="bg-primary-1 p-1 rounded-lg w-fit mx-3 lg:mx-0">
-				<div className="bg-white p-4 rounded-lg flex flex-col items-center align-middle justify-center">
-					<div className="px-10">
-						<Image
-							src="/images/logo.png"
-							alt="One Acre Fund Logo"
-							width={200}
-							height={50}
-							className="w-fit justify-center h-[50px] mx-auto flex flex-row "
-						/>
-						<p className="text-[16px] text-[#141414] font-semibold text-center">
-							Login to access your admin dashboard
-						</p>
-						<p className="text-sm text-[#7D7C81] text-center">
-							Enter your details to continue{" "}
-						</p>
-					</div>
-
-					<div
-						className="mt-8 mb-5 rounded-lg bg-[#9F9E9E29] shadow-inner p-2 w-full mx-4 flex flex-row justify-between items-center cursor-pointer"
-						onClick={handleGoogleSignIn}>
-						<div className="flex flex-row justify-start items-center gap-2">
+		<div className="flex bg-white flex-row justify-center lg:justify-between align-middle p-2 w-full">
+			<div className="rounded-lg w-[50%] lg:w-full hidden lg:block">
+				<Image
+					src="/images/logbg.png"
+					alt="login"
+					width={600}
+					height={700}
+					className="object-cover w-full h-full rounded-lg"
+				/>
+			</div>
+			<div className="farmlog w-[50%] lg:w-full">
+				<div className="bg-[#F0EFEF] shadow-md p-1 rounded-lg  mx-10 lg:mx-20 mt-[25%]">
+					<div className="bg-white shadow-md p-4 rounded-lg flex flex-col items-center align-middle justify-center">
+						<div className="px-10">
 							<Image
-								src="/images/google.png"
-								alt="Google Logo"
-								width={20}
-								height={20}
+								src="/images/logo.png"
+								alt="One Acre Fund Logo"
+								width={200}
+								height={50}
+								className="w-fit justify-center h-[50px] mx-auto flex flex-row "
 							/>
-							<p className="text-sm text-[#141414] font-normal font-inter">
-								Continue with Google
+							<p className="text-[16px] text-[#141414] font-semibold text-center">
+								Login to access your admin dashboard
+							</p>
+							<p className="text-sm text-[#7D7C81] text-center">
+								Enter your details to continue{" "}
 							</p>
 						</div>
-						<IconArrowRight color="#292D32" />
+
+						<div
+							className="mt-8 mb-5 rounded-lg bg-[#9F9E9E29] shadow-inner p-2 w-full mx-4 flex flex-row justify-between items-center cursor-pointer"
+							onClick={handleGoogleSignIn}>
+							<div className="flex flex-row justify-start items-center gap-2">
+								<Image
+									src="/images/google.png"
+									alt="Google Logo"
+									width={20}
+									height={20}
+								/>
+								<p className="text-sm text-[#141414] font-normal font-inter">
+									Continue with Google
+								</p>
+							</div>
+							<IconArrowRight color="#292D32" />
+						</div>
 					</div>
-				</div>
-				<div className="bg-primary p-3 items-center justify-center flex flex-row gap-1">
-					<p className="text-white text-sm font-light">
-						Experiencing issues signing in?{" "}
-					</p>
-					<Link href="/" className="text-white text-sm font-light underline">
-						{" "}
-						Contact Support
-					</Link>
+					<div className="p-3 items-center bg-transparent justify-center flex flex-row gap-1">
+						<p className="text-[#7D7C81] text-sm font-light">
+							Experiencing issues signing in?{" "}
+						</p>
+						<Link
+							href="/"
+							className="text-primary-1 text-sm font-light underline">
+							{" "}
+							Contact Support
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
